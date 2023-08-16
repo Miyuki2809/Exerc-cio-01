e3494b2170c689f1e73584c4ccfeec85ee95a476
Miyuki Hirata <mikioaraujohirata@gmail.com>
2023-08-16 18:56:31 -0300

    Programação

---
+++ src/exercicio01.java
@@ -0,0 +1,15 @@
+public class exercicio01 {
+    public static void main(String[] args) throws Exception {
+    int numeros = 1;
+	int soma = 0;
+	
+	while (numeros<=50) {
+		soma+=numeros;
+		numeros+=1;
+		}
+	System.out.println("A soma dos 50 primeirios nùmeros: "+soma);
+	}
+}
+
+    
+
